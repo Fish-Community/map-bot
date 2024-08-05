a36a46bb8ffdd9aec6a69489c57aa29c8a61aaee
juror #9 <64419198+Jurorno9@users.noreply.github.com>
2024-08-05 08:02:17 -0400

    Delete build directory

--- build/commands/ping.js
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-export async function ping(interaction) {
-    await interaction.reply('pong');
-}
-export const pingCommand = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong');
