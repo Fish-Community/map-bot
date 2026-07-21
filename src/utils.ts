f256f8041a1b82f55172db70c317bc3c274aa9f9
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2026-07-21 22:27:30 +0530

    attempt to fix hanging

--- src/utils.ts
+++ src/utils.ts
@@ -162,7 +162,9 @@ export async function runFunction(interaction: CommandInteraction, callback: () 
 		return await interaction.followUp(successMessage);
 	} catch (err) {
 		if (err instanceof Fail) {
-			return interaction.followUp(`Error: ${err.message}`);
+			return interaction.reply(`Error: ${err.message}`).catch(e =>
+				interaction.followUp(`Error: ${err.message}`)
+			);
 		} else throw err;
 	}
 }
